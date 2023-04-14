dc37c5d564f4637de79a6febae06d7d676ff90b0
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2023-04-14 07:18:17 -0700

    Add files via upload

---
+++ automcserver.py
@@ -0,0 +1,300 @@
+#!/usr/bin/python3
+
+print("Auto Minecraft Server by Enderbyte Programs (c) 2023")
+
+VERSION_MANIFEST = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
+
+
+print("Loading libraries:")
+import sys
+import os
+from time import sleep
+import curses
+import curses.textpad
+import json
+import re
+import datetime
+import subprocess
+sleep(0.1)
+print("Checking for libcursesplus")
+try:
+    import cursesplus
+except:
+    lop = os.system("pip3 install --quiet cursesplus")
+    if lop != 0:
+        print("Failed to install cursesplus")
+        sys.exit(1)
+    import cursesplus
+import cursesplus.messagebox
+import cursesplus.filedialog
+print("Checking for librequests")
+try:
+    import requests
+except:
+    lop = os.system("pip3 install --quiet requests")
+    if lop != 0:
+        print("Failed to install requests")
+        sys.exit(1)
+    import requests
+print("Checking for urllib3")
+try:
+    import urllib.request
+except:
+    lop = os.system("pip3 install --quiet urllib")
+    if lop != 0:
+        print("Failed to install urllib")
+        sys.exit(1)
+    import urllib.request
+import urllib.error
+print("Checking internet connection")
+
+def internet_on():
+    try:
+        urllib.request.urlopen('http://google.com', timeout=5)
+        return True
+    except urllib.error.URLError as err: 
+        return False
+APPDATADIR = os.path.expanduser("~/.local/share/mcserver")
+SERVERSDIR = APPDATADIR + "/servers"
+if not os.path.isdir(SERVERSDIR):
+    os.mkdir(SERVERSDIR)
+__DEFAULTAPPDATA__ = {
+    "servers" : [
+
+    ],
+    "hasCompletedOOBE" : False
+}
+def parse_size(data: int) -> str:
+    if data < 0:
+        neg = True
+        data = -data
+    else:
+        neg = False
+    if data < 2000:
+        result = f"{data} bytes"
+    elif data > 2000000000:
+        result = f"{round(data/1000000000,2)} GB"
+    elif data > 2000000:
+        result = f"{round(data/1000000,2)} MB"
+    elif data > 2000:
+        result = f"{round(data/1000,2)} KB"
+    if neg:
+        result = "-"+result
+    return result
+def get_java_version() -> str:
+    return subprocess.check_output(r"java -version 2>&1 | grep -Eow '[0-9]+\.[0-9]+' | head -1",shell=True).decode().strip()
+
+class PropertiesParse:
+    @staticmethod
+    def load(s: str) -> dict:
+        dat = s.splitlines()
+        dat = [d for d in dat if d.replace(" ","") != ""]
+        dat = [d for d in dat if d[0] != "#"]
+        final = {}
+        for d in dat:
+            key = d.split("=")[0]
+            val = "=".join(d.split("=")[1:])
+            final[key] = val
+        return final
+    @staticmethod
+    def dump(d: dict) -> str:
+        l = ""
+        for f in d.items():
+            l += f[0] + "=" + f[1] + "\n"
+        return l
+def setupnewserver(stdscr):
+    serversoftware = cursesplus.displayops(stdscr,["Vanilla","Cancel"],"Please choose your server software")
+    if serversoftware == 1:
+        return
+    elif serversoftware == 0:
+        stdscr.clear()
+        downloadversion = cursesplus.displayops(stdscr,["Cancel"]+[v["id"] for v in VERSION_MANIFEST_DATA["versions"]],"Please choose a version")
+        if downloadversion == 0:
+            return
+        else:
+            stdscr.clear()
+            cursesplus.displaymsgnodelay(stdscr,["Getting version manifest..."])
+            PACKAGEDATA = requests.get(VERSION_MANIFEST_DATA["versions"][downloadversion-1]["url"]).json()
+            cursesplus.displaymsgnodelay(stdscr,["Preparing new server"])
+            while True:
+                servername = cursesplus.cursesinput(stdscr,"What is the name of your server?").strip()
+                if not os.path.isdir(SERVERSDIR):
+                    os.mkdir(SERVERSDIR)
+                if os.path.isdir(SERVERSDIR+"/"+servername):
+                    cursesplus.displaymsg(stdscr,["Name already exists."])
+                    continue
+                else:
+                    try:
+                        os.mkdir(SERVERSDIR+"/"+servername)
+                    except:
+                        cursesplus.displaymsg(stdscr,["Error","Server path is illegal"])
+                    else:
+                        break
+    S_INSTALL_DIR = SERVERSDIR+"/"+servername
+    p = cursesplus.ProgressBar(stdscr,6,1,True,True,"Setting up server")
+    p.step("Getting download data",True)
+    S_DOWNLOAD_data = PACKAGEDATA["downloads"]["server"]
+    S_DOWNLOAD_size = parse_size(S_DOWNLOAD_data["size"])
+    p.appendlog(f"SIZE: {S_DOWNLOAD_size}")
+    p.appendlog(f"URL: {S_DOWNLOAD_data['url']}")
+    p.step("Downloading",True)
+    urllib.request.urlretrieve(S_DOWNLOAD_data["url"],S_INSTALL_DIR+"/server.jar")
+    p.step("Checking stuff",True)
+    setupeula = cursesplus.messagebox.askyesno(stdscr,["To proceed, you must agree","To Mojang's EULA","","Do you agree?"])
+    if setupeula:
+        with open(S_INSTALL_DIR+"/eula.txt","w+") as f:
+            f.write("eula=true")# Agree
+    p.step("Loading java",True)
+    usecustomjavaversion = cursesplus.askyesno(stdscr,["Do you want to use the default java install (program java)?",f"Version {get_java_version()}"])
+    if not usecustomjavaversion:
+        while True:
+            njavapath = cursesplus.filedialog.openfiledialog(stdscr,"Please choose a new java executable",directory="/")
+            if os.system(njavapath+" -version &> /dev/null") != 0:
+                cursesplus.displaymsg(stdscr,["Bad java file"])
+            else:
+                break
+    else:
+        njavapath = "java"
+    p.step("Preparing scripts",True)
+    while True:
+        memorytoall: str = cursesplus.cursesinput(stdscr,"How much memory should the server get? (EX: 1024M, 5G)")
+        if memorytoall.endswith("M") or memorytoall.endswith("G"):
+            try:
+                l = int(memorytoall[0:-1])
+                if (memorytoall.endswith("M") and l < 512) or (memorytoall.endswith("G") and l < 1):
+                    raise Exception()
+            except:
+                continue
+            else:
+                break
+        else:
+            continue
+    njavapath = njavapath.replace("//","/")
+    __SCRIPT__ = f"#!/usr/bin/sh\n{njavapath} -jar -Xms{memorytoall} -Xmx{memorytoall} {S_INSTALL_DIR}/server.jar nogui"
+    with open(S_INSTALL_DIR+"/start","w+") as f:
+        f.write(__SCRIPT__)
+    os.system(f"chmod +x '{S_INSTALL_DIR+'/start'}'")
+    p.step("All done!",True)
+    startnow = cursesplus.askyesno(stdscr,["Do you want to start your server now","to generate remaining config files","and to generate a default world?"])
+    APPDATA["servers"].append({"name":servername,"javapath":njavapath,"memory":memorytoall,"dir":S_INSTALL_DIR,"version":PACKAGEDATA["id"]})
+    updateappdata()
+    if not startnow:
+        return
+    curses.reset_shell_mode()
+    _OLDDIR = os.getcwd()
+    os.chdir(S_INSTALL_DIR)
+    os.system(f"'{S_INSTALL_DIR}/start'")
+    
+    curses.reset_prog_mode()
+    os.chdir(_OLDDIR)
+    stdscr.clear()
+    
+def servermgrmenu(stdscr):
+    stdscr.clear()
+    global APPDATA
+    chosenserver = cursesplus.displayops(stdscr,["Back"]+[a["name"] for a in APPDATA["servers"]],"Please choose a server")
+    if chosenserver == 0:
+        return
+    else:
+        _sname = [a["name"] for a in APPDATA["servers"]][chosenserver-1]
+        if os.path.isdir(SERVERSDIR+"/"+_sname):
+            SERVER_DIR = SERVERSDIR+"/"+_sname
+            _ODIR = os.getcwd()
+            os.chdir(SERVER_DIR)
+
+            #Manager server
+            while True:
+                w = cursesplus.displayops(stdscr,["Back","Start Server","Change MOTD","Advanced config","Delete server"])
+                if w == 0:
+                    break
+                elif w == 1:
+                    stdscr.clear()
+                    stdscr.addstr(0,0,f"STARTING {str(datetime.datetime.now())[0:-5]}")
+                    stdscr.refresh()
+                    curses.reset_shell_mode()
+                    os.system("./start")
+                    curses.reset_prog_mode()
+                    stdscr.clear()
+                    stdscr.refresh()
+                elif w == 2:
+                    if not os.path.isfile("server.properties"):
+                        cursesplus.displaymsg(stdscr,["ERROR","server.properties could not be found","Try starting your sever to generate one"])
+                    else:
+                        with open("server.properties") as f:
+                            config = PropertiesParse.load(f.read())
+                        cursesplus.displaymsg(stdscr,["Current Message Is",config["motd"]])
+                        newmotd = cursesplus.cursesinput(stdscr,"Please input a new MOTD")
+                        config["motd"] = newmotd
+                        with open("server.properties","w+") as f:
+                            f.write(PropertiesParse.dump(config))
+
+        else:
+            cursesplus.displaymsg(stdscr,["ERROR","Server not found"])
+
+def updateappdata():
+    global APPDATA
+    global APPDATAFILE
+    with open(APPDATAFILE,"w+") as f:
+        f.write(json.dumps(APPDATA))
+
+def main(stdscr):
+    global VERSION_MANIFEST
+    global VERSION_MANIFEST_DATA
+    global APPDATAFILE
+    try:
+        cursesplus.displaymsgnodelay(stdscr,["Auto Minecraft Server","Starting..."])
+        cursesplus.load_colours()
+        global APPDATA
+        APPDATAFILE = os.path.expanduser("~/.local/share/mcserver")+"/config.json"
+        if not os.path.isdir(os.path.expanduser("~/.local/share/mcserver")):
+            os.mkdir(os.path.expanduser("~/.local/share/mcserver"))
+        if not os.path.isfile(APPDATAFILE):
+            with open(APPDATAFILE,"w+") as f:
+                f.write(json.dumps(__DEFAULTAPPDATA__))
+            APPDATA = __DEFAULTAPPDATA__
+        else:
+            try:
+                with open(APPDATAFILE) as f:
+                    APPDATA = json.load(f)
+            except:
+                with open(APPDATAFILE,"w+") as f:
+                    f.write(json.dumps(__DEFAULTAPPDATA__))
+                APPDATA = __DEFAULTAPPDATA__
+        #Graphics support loading
+        if not internet_on():
+            if not cursesplus.messagebox.askyesno(stdscr,["WARNING","No internet connection could be found!","You may run in to errors","Are you sure you want to continue?"]):
+                return
+        VERSION_MANIFEST_DATA = requests.get(VERSION_MANIFEST).json()
+        
+        setupservernow = False
+        if not APPDATA["hasCompletedOOBE"]:
+            stdscr.clear()
+            cursesplus.displaymsg(stdscr,["AMCS OOBE","","Welcome to AutoMinecraftServer: The best way to make a Minecraft server","This guide will help you set up your first Minecraft Server","","v1.0"])
+            cursesplus.cursestransition(stdscr,type=1)
+            setupservernow = cursesplus.askyesno(stdscr,["AMCS OOBE","","Would you like to set up a server now?"])
+            stdscr.clear()
+            if setupservernow:
+                cursesplus.cursestransition(stdscr,type=0)
+                setupnewserver(stdscr)
+
+        APPDATA["hasCompletedOOBE"] = True
+        updateappdata()
+        
+        while True:
+            m = cursesplus.displayops(stdscr,["Set up new server","View list of servers","Quit"],"AMCS 1.0")
+            if m == 2:
+                cursesplus.cursestransition(stdscr,type=0)
+                return
+            elif m == 0:
+                cursesplus.cursestransition(stdscr,type=0)
+                setupnewserver(stdscr)
+            elif m == 1:
+                servermgrmenu(stdscr)
+    except KeyboardInterrupt:
+        if cursesplus.messagebox.askyesno(stdscr,["Are you sure you want to quit?"]):
+            sys.exit()
+    except Exception as e:
+        cursesplus.displaymsg(stdscr,["An error occured",str(e)])
+
+
+curses.wrapper(main)
---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/sh
+#This program installs automcserver. Run with --help for help or --local to install locally
+
+if [ "$1" == "--remove" ] || [ "$1" == "-r" ]; then
+    #Remove locally
+    echo "Removing locally..."
+    rm ~/.local/bin/automcserver 2>/dev/null
+    rm ~/.local/bin/mcserver 2>/dev/null
+    echo "Removing globally..."
+    if [ "$EUID" -ne 0 ]
+        then echo "WARNING: Root priviliges are required to uninstall globally"
+    fi
+    exit 0
+fi
+
+FILE=automcserver.py
+if ! test -f "$FILE"; then
+    #Download file
+    if ! command -v wget &>/dev/null; then
+        echo "wget is required to do a download install."
+        exit -1
+    fi
+    if ! command -v gzip &>/dev/null; then
+        echo "GZIP is required to do a download install"
+        exit -1
+    fi
+    wget -q -O automcserver.py.gz http://enderbyteprograms.ddnsfree.com:10223/downloads/hosted/automcserver.py.gz
+    gzip -df automcserver.py.gz
+fi
+
+if ! command -v python3 &>/dev/null; then
+    echo "Please install Python3 before using this program"
+    exit -1
+fi
+
+if ! command -v pip3 &>/dev/null; then
+    echo "Please install python3-pip before using this program"
+    exit -1
+fi
+if ! command -v java &>/dev/null; then
+    read -p "WARNING: You need java to run a Minecraft server. Are you sure you want to go ahead with the install?" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Nn]$ ]]
+        then
+            exit 1
+    fi
+fi
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    echo "Auto Minecraft Server Installer by Enderbyte Programs (c) 2023"
+    echo "Usage: install.sh [Arguments]"
+    echo "-h --help :       Displays this help menu"
+    echo "-l --local :      Installs program locally (doesn't need root)"
+    echo "-r --remove :     Removes program both globally and locally"
+    echo " (no arguments) : Installs program globally (requires root)"
+    exit 0
+fi
+
+if [ "$1" == "--local" ] || [ "$1" == "-l" ]; then
+    cp $FILE ~/.local/bin/automcserver #Copy file
+    chmod +x ~/.local/bin/automcserver # Allow execution
+    if ! test -f "$HOME/.local/bin/mcserver"; then
+        ln ~/.local/bin/automcserver ~/.local/bin/mcserver #Create link
+    fi
+    
+    echo "Successfully installed AutoMcServer Locally"
+
+else
+    if [ "$EUID" -ne 0 ]
+        then echo "Please run as root or run with --local argument"
+        exit -1
+    fi
+    cp $FILE /usr/bin/automcserver # Copy file
+    chmod +x /usr/bin/automcserver #Allow execution
+    if ~ test -f "/usr/bin/mcserver"; then
+        ln /usr/bin/automcserver /usr/bin/mcserver #Create link
+    fi
+    echo "Sucessfully installed Auto Minecraft Server"
+fi
