e6d4eddee020e66866195b25e11e37013198597f
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2023-04-16 11:17:53 -0700

    Add files via upload

--- automcserver.py
+++ automcserver.py
@@ -6,16 +6,16 @@ VERSION_MANIFEST = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.j
 
 
 print("Loading libraries:")
+import shutil
 import sys
 import os
 from time import sleep
 import curses
 import curses.textpad
 import json
-import re
+
 import datetime
 import subprocess
-sleep(0.1)
 print("Checking for libcursesplus")
 try:
     import cursesplus
@@ -56,6 +56,8 @@ def internet_on():
         return False
 APPDATADIR = os.path.expanduser("~/.local/share/mcserver")
 SERVERSDIR = APPDATADIR + "/servers"
+if not os.path.isdir(APPDATADIR):
+    os.mkdir(APPDATADIR)
 if not os.path.isdir(SERVERSDIR):
     os.mkdir(SERVERSDIR)
 __DEFAULTAPPDATA__ = {
@@ -82,7 +84,10 @@ def parse_size(data: int) -> str:
         result = "-"+result
     return result
 def get_java_version() -> str:
-    return subprocess.check_output(r"java -version 2>&1 | grep -Eow '[0-9]+\.[0-9]+' | head -1",shell=True).decode().strip()
+    try:
+        return subprocess.check_output(r"java -version 2>&1 | grep -Eow '[0-9]+\.[0-9]+' | head -1",shell=True).decode().strip()
+    except:
+        return "Error"
 
 class PropertiesParse:
     @staticmethod
@@ -103,11 +108,13 @@ class PropertiesParse:
             l += f[0] + "=" + f[1] + "\n"
         return l
 def setupnewserver(stdscr):
+    stdscr.erase()
     serversoftware = cursesplus.displayops(stdscr,["Vanilla","Cancel"],"Please choose your server software")
     if serversoftware == 1:
         return
     elif serversoftware == 0:
         stdscr.clear()
+        stdscr.erase()
         downloadversion = cursesplus.displayops(stdscr,["Cancel"]+[v["id"] for v in VERSION_MANIFEST_DATA["versions"]],"Please choose a version")
         if downloadversion == 0:
             return
@@ -145,7 +152,9 @@ def setupnewserver(stdscr):
         with open(S_INSTALL_DIR+"/eula.txt","w+") as f:
             f.write("eula=true")# Agree
     p.step("Loading java",True)
-    usecustomjavaversion = cursesplus.askyesno(stdscr,["Do you want to use the default java install (program java)?",f"Version {get_java_version()}"])
+    usecustomjavaversion = cursesplus.messagebox.askyesno(stdscr,["Do you want to use the default java install (program java)?",f"Version {get_java_version()}"])
+    stdscr.clear()
+    stdscr.erase()
     if not usecustomjavaversion:
         while True:
             njavapath = cursesplus.filedialog.openfiledialog(stdscr,"Please choose a new java executable",directory="/")
@@ -175,7 +184,7 @@ def setupnewserver(stdscr):
         f.write(__SCRIPT__)
     os.system(f"chmod +x '{S_INSTALL_DIR+'/start'}'")
     p.step("All done!",True)
-    startnow = cursesplus.askyesno(stdscr,["Do you want to start your server now","to generate remaining config files","and to generate a default world?"])
+    startnow = cursesplus.messagebox.askyesno(stdscr,["Do you want to start your server now","to generate remaining config files","and to generate a default world?"])
     APPDATA["servers"].append({"name":servername,"javapath":njavapath,"memory":memorytoall,"dir":S_INSTALL_DIR,"version":PACKAGEDATA["id"]})
     updateappdata()
     if not startnow:
@@ -227,6 +236,31 @@ def servermgrmenu(stdscr):
                         config["motd"] = newmotd
                         with open("server.properties","w+") as f:
                             f.write(PropertiesParse.dump(config))
+                elif w == 3:
+                    if not os.path.isfile("server.properties"):
+                        cursesplus.displaymsg(stdscr,["ERROR","server.properties could not be found","Try starting your sever to generate one"])
+                    else:
+                        with open("server.properties") as f:
+                            config = PropertiesParse.load(f.read())
+                        while True:
+                            chc = cursesplus.optionmenu(stdscr,["BACK"]+[f for f in list(config.keys())])
+                            if chc == 0:
+                                break
+                            else:
+                                cursesplus.displaymsg(stdscr,["Current value of Is",list(config.values())[chc-1]])
+                                newval = cursesplus.cursesinput(stdscr,f"Please input a new value for {list(config.keys())[chc-1]}")
+                                config[list(config.keys())[chc-1]] = newval
+                        with open("server.properties","w+") as f:
+                            f.write(PropertiesParse.dump(config))
+                elif w == 4:
+                    if cursesplus.messagebox.askyesno(stdscr,["Are you sure that you want to delete this server?","It will be GONE FOREVER"]):
+                        if cursesplus.messagebox.askyesno(stdscr,["Are you sure that you want to delete this server?","It will be GONE FOREVER","THIS IS YOUR LAST CHANCE"]):
+                            os.chdir(SERVERSDIR)
+                            shutil.rmtree(SERVER_DIR)
+                            del APPDATA["servers"][chosenserver-1]
+                            cursesplus.messagebox.showinfo(stdscr,["Deleted server"])
+                            stdscr.clear()
+                            break
 
         else:
             cursesplus.displaymsg(stdscr,["ERROR","Server not found"])
@@ -242,8 +276,9 @@ def main(stdscr):
     global VERSION_MANIFEST_DATA
     global APPDATAFILE
     try:
+        curses.start_color()
+
         cursesplus.displaymsgnodelay(stdscr,["Auto Minecraft Server","Starting..."])
-        cursesplus.load_colours()
         global APPDATA
         APPDATAFILE = os.path.expanduser("~/.local/share/mcserver")+"/config.json"
         if not os.path.isdir(os.path.expanduser("~/.local/share/mcserver")):
@@ -270,23 +305,25 @@ def main(stdscr):
         if not APPDATA["hasCompletedOOBE"]:
             stdscr.clear()
             cursesplus.displaymsg(stdscr,["AMCS OOBE","","Welcome to AutoMinecraftServer: The best way to make a Minecraft server","This guide will help you set up your first Minecraft Server","","v1.0"])
-            cursesplus.cursestransition(stdscr,type=1)
-            setupservernow = cursesplus.askyesno(stdscr,["AMCS OOBE","","Would you like to set up a server now?"])
+            
+            setupservernow = cursesplus.messagebox.askyesno(stdscr,["AMCS OOBE","","Would you like to set up a server now?"])
             stdscr.clear()
             if setupservernow:
-                cursesplus.cursestransition(stdscr,type=0)
+                
                 setupnewserver(stdscr)
 
         APPDATA["hasCompletedOOBE"] = True
         updateappdata()
-        
+        mx,my = os.get_terminal_size()
+        if mx < 120 or my < 20:
+            cursesplus.messagebox.showwarning(stdscr,["Your terminal size may be too small","Some instability may occur","For best results, set size to","at least 120x20"])
         while True:
             m = cursesplus.displayops(stdscr,["Set up new server","View list of servers","Quit"],"AMCS 1.0")
             if m == 2:
-                cursesplus.cursestransition(stdscr,type=0)
+
                 return
             elif m == 0:
-                cursesplus.cursestransition(stdscr,type=0)
+
                 setupnewserver(stdscr)
             elif m == 1:
                 servermgrmenu(stdscr)
