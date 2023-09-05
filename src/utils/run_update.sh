f8aa43891cad707426a7aec55130270216173d5f
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2023-09-05 16:33:31 -0700

    Add files via upload

---
+++ src/utils/run_update.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+set -e
+if [ -d "/tmp/amcsupdate" ]; then
+    rm -rf /tmp/amcsupdate
+fi
+mkdir /tmp/amcsupdate
+
+if ! command -v tar &> /dev/null
+then
+    echo "tar is required to use this program"
+    exit 1
+fi
+if ! command -v xz &> /dev/null
+then
+    echo "xz is required to use this"
+    exit 1
+fi
+if ! command -v git &> /dev/null
+then
+    echo "git is required to update."
+    exit 1
+fi
+
+pushd /tmp/amcsupdate
+git clone https://github.com/Enderbyte-Programs/CraftServerSetup
+cd CraftServerSetup 
+bash scripts/install.sh
+popd
+echo "CraftServerSetup updated successfully. Restart it with the amcs command."
