cacfe3072dd45698df957b81ca6da67e02cccb18
Jordan Rahim <83879635+Enderbyte09@users.noreply.github.com>
2023-10-14 10:09:26 -0700

    deb test 2

--- scripts/debian/postinstall.sh
+++ scripts/debian/postinstall.sh
@@ -3,3 +3,20 @@
 #This program finishes the installer.
 
 #TODO: Install MIME type
+FILEREGFILE="$HOME/.config/mimeapps.list"
+MIMEDIR="$HOME/.local/share/mime/packages"
+
+if [ -f $FILEREGFILE ]; then
+    if ! grep -q "minecraft-server" "$FILEREGFILE"; then
+        echo "application/minecraft-server=craftserversetup.desktop" >> $FILEREGFILE
+    fi
+fi
+
+if [ ! -d $MIMEDIR ]; then
+    mkdir -p $MIMEDIR
+fi
+if [ ! -f $MIMEDIR/craftserversetup.xml ]; then
+    cp /usr/lib/craftserversetup/tempdeb $MIMEDIR/craftserversetup.xml
+fi
+
+rm -rf /usr/lib/craftserversetup/tempdeb
