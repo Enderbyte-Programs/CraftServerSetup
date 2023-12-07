321e506bc5eceb1aa0a3a5fc32a8596fbf74bd97
Jordan Rahim <83879635+Enderbyte09@users.noreply.github.com>
2023-12-07 09:26:25 -0800

    add uninstall

---
+++ universalwindowsinstaller/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CRSSAutoInstall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(269, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(506, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download and Install";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1055, 73);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CraftServerSetup Universal Installer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(875, 100);
+            this.label2.TabIndex = 2;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(781, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(246, 65);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Download only";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(237, 66);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Cancel and quit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 289);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1001, 42);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 355);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Status: Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 389);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Craft Server Setup Universal Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ universalwindowsinstaller/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CRSSAutoInstall
+{
+    public partial class Form1 : Form
+    {
+        private bool downloaddone = false;
+        private bool proceed = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            proceed = false;
+            startDownload();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void startDownload()
+        {
+            downloaddone = false;
+            label3.Text = "Resolving version (the installer may freeze for a second)";
+            progressBar1.Value = 0;
+            label3.Invalidate();
+            label3.Refresh();
+            this.Refresh();
+            WebClient client = new WebClient();
+            String downloadedString = client.DownloadString("https://github.com/Enderbyte-Programs/CraftServerSetup/raw/main/update.txt").Split('|')[0];
+            Thread thread = new Thread(() => {
+                client = new WebClient();
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                client.DownloadFileAsync(new Uri(downloadedString),Environment.ExpandEnvironmentVariables("%USERPROFILE%\\Downloads\\CraftServerSetup-setup.exe"));
+            });
+            thread.Start();
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                double bytesIn = double.Parse(e.BytesReceived.ToString());
+                double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = bytesIn / totalBytes * 100;
+                label3.Text = "Downloaded " + e.BytesReceived + " of " + e.TotalBytesToReceive;
+                progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            });
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            downloaddone = true;
+            this.BeginInvoke((MethodInvoker)delegate {
+                label3.Text = "Download Completed";
+                if (proceed)
+                {
+                    label3.Text = "Running main installer";
+                    Refresh();
+                    Process p = new Process();
+                    p.StartInfo.FileName = Environment.ExpandEnvironmentVariables("%USERPROFILE%\\Downloads\\CraftServerSetup-setup.exe");
+                    p.Start();
+                    Environment.Exit(0);
+                }
+            });
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            proceed = true;
+            startDownload();
+           
+            
+            
+        }
+    }
+}
