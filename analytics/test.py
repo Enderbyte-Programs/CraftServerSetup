b3aeea6cfa0ac5d52a035958a676e637e7f91abe
Jordan Rahim <83879635+Enderbyte09@users.noreply.github.com>
2024-07-04 11:19:39 -0700

    Decomission old telemetry server

--- analytics/test.py
+++
@@ -1,8 +0,0 @@
-import socket
-import platform
-
-s = socket.socket()
-#s.connect(('enderbyteprograms.ddnsfree.com',11111))
-s.connect(('localhost',11111))
-s.sendall(f"GET /api/amcs/os={platform.platform()}&ver=0.12.2 HTTP/1.1".encode())
-s.close()
