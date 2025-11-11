84e36cc9d6b718923b2f3419837f42dedadfb5ac
enderbyte09 <enderbyte09@gmail.com>
2025-11-10 18:28:37 -0800

    remove minifier

--- scripts/minify.py
+++
@@ -1,9 +0,0 @@
-import sys
-import python_minifier
-infile = sys.argv[1]
-outfile = sys.argv[2]
-with open(infile) as f:
-    data = f.read()
-o = python_minifier.minify(data,rename_locals=True,rename_globals=True)
-with open(outfile,"w+") as f:
-    f.write(o)
